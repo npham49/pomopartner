b5e3e055284fd10fa7bee19e5dcfbb5c12fa563f
npham49 <nguyenphamswork@gmail.com>
2024-03-17 12:45:17 -0700

    addedlinters

---
+++ bin/git_hooks.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: depend_on_referenced_packages
+
+import "package:dart_pre_commit/dart_pre_commit.dart";
+import "package:git_hooks/git_hooks.dart";
+
+void main(List<String> arguments) {
+  final params = {Git.preCommit: _preCommit};
+  GitHooks.call(arguments, params);
+}
+
+Future<bool> _preCommit() async {
+  final result = await DartPreCommit.run();
+  return result.isSuccess;
+}
