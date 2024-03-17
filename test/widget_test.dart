79da9ab8dc85ee878fa1be7785d784a7201d3183
npham49 <nguyenphamswork@gmail.com>
2024-03-17 16:49:09 -0700

    added basic styling

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,16 +4,25 @@
 // utility in the flutter_test package. For example, you can send tap and scroll
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
+import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:json_theme/json_theme.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:pomopartner/main.dart';
 
-void main() {
+void main() async {
+  SchemaValidator.enabled = false;
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final themeStr = await rootBundle.loadString('assets/appainter_theme.json');
+  final themeJson = jsonDecode(themeStr);
+  final theme = ThemeDecoder.decodeThemeData(themeJson)!;
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(MyApp(theme: theme));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
